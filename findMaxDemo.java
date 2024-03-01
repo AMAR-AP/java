f919c0a5374f9b06d4867029d91dee9d7d34a3d6
AMAR-AP <114557114+AMAR-AP@users.noreply.github.com>
2024-03-01 10:05:59 +0530

    Create findMaxDemo.java

---
+++ findMaxDemo.java
@@ -0,0 +1,19 @@
+package array;
+public class findMaxDemo{
+  static int findMax(int a[]){
+  int max=a[0];
+  for(int i=1;i<a.length;i++)
+  
+  {
+    if (max<a[i])
+    max=a[i];
+   
+  }
+  return max;
+}
+public static void main(String[] args)
+{
+  int a[]={10,2,7,4,9};
+  System.out.println("The max value is:"+findMax(a));
+}
+}
